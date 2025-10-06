3e0fff6b33f3c31ad38143515cde13e9d2d298f3
Anil-canim <anilcanguler@ogr365.iyte.edu.tr>
2025-10-06 22:29:27 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+s="ABC d3f ghi"
+print(s[11::-1])
+
+a="car"
+
+k=a[0:3]
+print(k)
+
+b=3
+c="hello"
+d="man"
+print(str(b), c, d)
+
+a =input("enter a word ")
+print('i can',a,'better than you ')
+print((a+" ")*5)
