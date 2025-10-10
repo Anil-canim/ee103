c1776d3736c3e2f34bdbf30af71934ceba004195
Anil-canim <anilcanguler@ogr365.iyteedu.tr>
2025-10-10 17:59:49 +0300

    test

---
+++ noooooo.py
@@ -0,0 +1,14 @@
+n="nooooooo"
+b=int(len(n))
+while b>2:
+    print(n)
+    b=b-1
+print("no")
+
+mysum=0
+start=3
+end=5
+for i in range(start,end+1):
+    print("i=",i)
+    mysum += i
+print(mysum)
