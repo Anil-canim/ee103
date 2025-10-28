f8f457bde1e3d6542056b45466991e6b57023baa
Anil-canim <anilcanguler@ogr365.iyte.edu.tr>
2025-10-28 14:33:19 +0300

    using for playground.py for now

---
+++ secret_number_library.py
@@ -0,0 +1,9 @@
+# secret_number_library.py is using random to generate a secret number and provide a hint.^
+
+import random
+#i use random library to generate a secret number
+
+secret_number=random.randint(10,20)
+#the range could be changeable
+hint="Try a number between 10 and 20."
+#this is the hint for the user
