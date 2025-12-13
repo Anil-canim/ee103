319babf7ee9f9e9449a69aa8f7383ac1387fd424
Anil-canim <anilcanguler@ogr365.iyte.edu.tr>
2025-12-13 10:25:40 +0300

    deleted bcs of unnecessary

--- finger_practice3.py
+++
@@ -1,7 +0,0 @@
-# finger_practice3.py
-# with while loop
-number=int(input("give us a number: "))
-i=0
-while i<number:
-    i +=1
-    print("hello world")
--- text_tools.py
+++
@@ -1,6 +0,0 @@
-def convert_to_upper(sentence):
-    return sentence.upper()
-
-def convert_to_title(sentence):
-    return sentence.title()
-
