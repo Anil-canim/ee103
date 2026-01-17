c70518fd45f7ad8155f14849dd44a7b3a80ec83a
Anil-canim <anilcanguler@ogr365.iyte.edu.tr>
2026-01-17 19:29:06 +0300

    fix(math): final fix for recursive logic

--- practice/finger_practice-15.py
+++ practice/finger_practice-15.py
@@ -1,10 +1,7 @@
 def recur_power(base, exp):
     if exp==0:
         return 1
-    if exp>1:
-        base=base*recur_power(base,exp-1)
+    base=base*recur_power(base,exp-1)
     return base
 
-print(recur_power(3,5))
-
-
+print(recur_power(45,0))
